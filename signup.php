3a8ec1b4109de51fa7d22ac9d3e779a05f67bc21
zuyinahuzaifa <zuyinasiddique11@gmail.com>
2025-02-04 08:59:14 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php
+require 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    if ($stmt->execute([$username, $email, $password])) {
+        echo "Registration successful. <a href='login.php'>Login here</a>";
+    } else {
+        echo "Error during registration.";
+    }
+}
+?>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Sign Up</button>
+</form>
